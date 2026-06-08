c5e44baec91aef8d554cf34dec9342d2eada13ae
Giran Oh <giranoh12@gmail.com>
2026-06-08 20:48:55 +0900

    feat(api): UserEntity 추가 최소 컬럼 설계

---
+++ apps/api/src/users/user.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+// TypeORM (@)데코레이터: 이 클래스와 속성을 DB 테이블/컬럼으로 매핑한다.
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  nickname!: string;
+
+  @Column()
+  passwordHash!: string;
+
+  @CreateDateColumn()
+  createAt!: Date;
+}
