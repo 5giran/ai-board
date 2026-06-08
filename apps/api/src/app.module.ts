ed5b0cb37bb37ff7c3f38ef78ed19e730008086e
Giran Oh <giranoh12@gmail.com>
2026-06-08 17:08:26 +0900

    feat(api): 앱 모듈에 유저 모듈 연결

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { createTypeOrmOptions } from './config/typeorm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { createTypeOrmOptions } from './config/typeorm.config';
     TypeOrmModule.forRootAsync({
       useFactory: () => createTypeOrmOptions(),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
