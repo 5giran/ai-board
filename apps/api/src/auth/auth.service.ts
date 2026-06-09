3fe46442135d2147f96be3fe3951fc5d7143fc1a
Giran Oh <giranoh12@gmail.com>
2026-06-09 20:29:12 +0900

    feat(api): AuthService 회원가입 로직 구현

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,5 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+import { UsersService } from '../users/users.service';
+import { SignupDto } from '../dto/signup.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+  async signup(dto: SignupDto) {
+    const existingEmail = await this.usersService.findByEmail(dto.email);
+
+    if (existingEmail) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const existingNickname = await this.usersService.findByNickname(
+      dto.nickname,
+    );
+
+    if (existingNickname) {
+      throw new ConflictException('Nickname already exists');
+    }
+
+    const passwordHash = await argon2.hash(dto.password);
+
+    return this.usersService.createUser(dto.email, dto.nickname, passwordHash);
+  }
 }
