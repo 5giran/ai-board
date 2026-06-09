c3f0b51a2890921d5dd589ebd43d6b7663758789
Giran Oh <giranoh12@gmail.com>
2026-06-09 19:41:13 +0900

    feat(api): UsersService repository 메서드 추가

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  private users = [
-    { id: 1, name: 'Vaporeon' },
-    { id: 2, name: 'Jolteon' },
-  ];
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
+  // findAll()은 그냥 아직 컨트롤러 수정이 안되어서 남겨둠.
   findAll() {
-    return this.users;
+    return this.userRepository.find();
+  }
+
+  findByEmail(email: string) {
+    return this.userRepository.findOne({
+      where: { email },
+    });
+  }
+
+  findByNickname(nickname: string) {
+    return this.userRepository.findOne({
+      where: { nickname },
+    });
+  }
+
+  createUser(email: string, nickname: string, passwordHash: string) {
+    const user = this.userRepository.create({
+      email,
+      nickname,
+      passwordHash,
+    });
+    return this.userRepository.save(user);
   }
 }
