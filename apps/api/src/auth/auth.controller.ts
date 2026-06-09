acf8514a845956c78e8118f87eb72ce08926ae33
Giran Oh <giranoh12@gmail.com>
2026-06-09 22:13:57 +0900

    feat(api): 로그인 로직 구현

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignupDto } from '../dto/signup.dto';
 import { LoginDto } from '../dto/login.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('signup')
   signup(@Body() dto: SignupDto) {
-    return 'signup';
+    return this.authService.signup(dto);
   }
 
   @Post('login')
   login(@Body() dto: LoginDto) {
-    return 'login';
+    return this.authService.login(dto);
   }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,11 +1,49 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import * as argon2 from 'argon2';
 import { UsersService } from '../users/users.service';
 import { SignupDto } from '../dto/signup.dto';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from '../dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(dto: LoginDto) {
+    const user = await this.usersService.findByEmail(dto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('인증 정보가 올바르지 않습니다.');
+    }
+
+    const isPasswordValid = await argon2.verify(
+      user.passwordHash,
+      dto.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('인증 정보가 올바르지 않습니다.');
+    }
+
+    const payload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return {
+      accessToken,
+    };
+  }
+
   async signup(dto: SignupDto) {
     const existingEmail = await this.usersService.findByEmail(dto.email);
 
